9543d30b4e486eb1219178305e89b87313069807
gautte <s362066@oslomet.no>
2023-11-11 14:45:39 +0100

    Lagt til innhold i about og contact

--- CabinFeverReact/ClientApp/src/components/pages/About.js
+++ CabinFeverReact/ClientApp/src/components/pages/About.js
@@ -4,7 +4,38 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            {/* Your About content */}
+            {/* Banner */}
+            <div className="banner-secondary">
+                <div className="banner-secondary-container">
+                    {/* Midlertidig */}
+                    <img src="https://images.pexels.com/photos/5365875/pexels-photo-5365875.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Banner" />
+                </div>
+                <div className="row">
+                    <div className="col-12 d-flex align-items-center justify-content-center position-absolute top-50 start-50 translate-middle">
+                        <div className="text-center">
+                            {/* The text */}
+                            <h1 className="text-white">About</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* About content */}
+            <div className="container my-5">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut etiam sit amet nisl purus. Tristique sollicitudin nibh sit amet commodo
+                    nulla facilisi. Tristique magna sit amet purus gravida. Aliquam id diam maecenas ultricies mi. Odio ut
+                    enim blandit volutpat maecenas volutpat blandit aliquam. Auctor elit sed vulputate mi. Mauris cursus
+                    mattis molestie a iaculis at erat. Varius sit amet mattis vulputate. Varius sit amet mattis vulputate.
+                    At imperdiet dui accumsan sit amet nulla facilisi morbi. Urna cursus eget nunc scelerisque viverra
+                    mauris in aliquam sem. Mi proin sed libero enim sed faucibus turpis in. Tristique senectus et netus et.
+                    Id velit ut tortor pretium viverra suspendisse potenti nullam. Vitae et leo duis ut diam quam. Facilisi
+                    cras fermentum odio eu feugiat pretium nibh ipsum consequat. Eget felis eget nunc lobortis.
+                </p>
+
+                {/* Add more paragraphs as needed */}
+            </div>
         </div>
     );
 };
--- CabinFeverReact/ClientApp/src/components/pages/Contact.js
+++ CabinFeverReact/ClientApp/src/components/pages/Contact.js
@@ -4,7 +4,77 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
-            {/* Your Contact content */}
+            {/* Banner */}
+            <div className="banner-secondary">
+                <div className="banner-secondary-container">
+                    {/* Midlertidig */}
+                    <img src="https://images.pexels.com/photos/5365875/pexels-photo-5365875.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" id="banner-image" alt="Banner" />
+                </div>
+                <div className="row">
+                    <div className="col-12 d-flex align-items-center justify-content-center position-absolute top-50 start-50 translate-middle">
+                        <div className="text-center">
+                            {/* The text */}
+                            <h1 className="text-white">Contact</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Contact information */}
+            <div className="container my-5">
+                <h2>Customers and renters</h2>
+                <p>Email: cabinfever.support@cf.no</p>
+                <p>Phone: 22 xx xx xx</p>
+            </div>
+
+            <div className="container my-5">
+                <h2>Cabin owners regarding guests or owned cabins</h2>
+                <p>Email: cabinfever.owner@cf.no</p>
+                <p>Phone: 23 xx xx xx</p>
+            </div>
+
+            <div className="container my-5">
+                <h2>Opening hours for phone support</h2>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Monday</td>
+                            <td></td>
+                            <td>9 AM - 5 PM</td>
+                        </tr>
+                        <tr>
+                            <td>Tuesday</td>
+                            <td></td>
+                            <td>9 AM - 5 PM</td>
+                        </tr>
+                        <tr>
+                            <td>Wednesday</td>
+                            <td></td>
+                            <td>9 AM - 5 PM</td>
+                        </tr>
+                        <tr>
+                            <td>Thursday</td>
+                            <td></td>
+                            <td>9 AM - 5 PM</td>
+                        </tr>
+                        <tr>
+                            <td>Friday</td>
+                            <td></td>
+                            <td>9 AM - 4 PM</td>
+                        </tr>
+                        <tr>
+                            <td>Saturday</td>
+                            <td></td>
+                            <td>10 AM - 3 PM</td>
+                        </tr>
+                        <tr>
+                            <td>Sunday</td>
+                            <td></td>
+                            <td>Closed</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
