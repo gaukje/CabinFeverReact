f0d2bc53cd178d9c75b53519e667b1af325b201d
gautte <s362066@oslomet.no>
2023-11-26 21:20:50 +0100

    Ryddet litt på koden

--- CabinFeverReact/Controllers/UserController.cs
+++ CabinFeverReact/Controllers/UserController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 using CabinFeverReact.Models;
 using CabinFeverReact.Services;
-using CabinFeverReact.DTO;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -45,72 +44,3 @@ public class UserController : ControllerBase
         return BadRequest();
     }
 }
-/*
-    private readonly UserManager<User> _userManager;
-private readonly SignInManager<User> _signInManager;
-
-public UserController(UserManager<User> userManager, SignInManager<User> signInManager)
-{
-    _userManager = userManager;
-    _signInManager = signInManager;
-}
-[HttpPost("register")]
-public async Task<IActionResult> Register([FromBody] RegisterDto model)
-{
-    var user = new User { UserName = model.Email, Email = model.Email };
-    var result = await _userManager.CreateAsync(user, model.Password);
-
-    if (result.Succeeded)
-    {
-        await _signInManager.SignInAsync(user, isPersistent: false);
-        return Ok(); // Or return a token if you're using JWT
-    }
-
-
-    foreach (var error in result.Errors)
-    {
-        ModelState.AddModelError(string.Empty, error.Description);
-    }
-
-    return BadRequest(ModelState);
-}
-
-[HttpPost("login")]
-public async Task<IActionResult> Login([FromBody] LoginDto model)
-{
-    var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
-
-    if (result.Succeeded)
-    {
-        // Generate JWT token here if using token-based authentication
-        return Ok(); // Return the token
-    }
-
-    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-    return BadRequest(ModelState);
-}
-
-[HttpPost("logout")]
-public async Task<IActionResult> Logout()
-{
-    await _signInManager.SignOutAsync();
-    return Ok();
-}
-
-// Example of generating a JWT token
-private string GenerateJwtToken(User user)
-{
-    var tokenHandler = new JwtSecurityTokenHandler();
-    var key = Encoding.ASCII.GetBytes("YourSecretKey"); // Replace with your key
-    var tokenDescriptor = new SecurityTokenDescriptor
-    {
-        Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id) }),
-        Expires = DateTime.UtcNow.AddDays(7),
-        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-    };
-    var token = tokenHandler.CreateToken(tokenDescriptor);
-    return tokenHandler.WriteToken(token);
-}
-}
-
-*/
