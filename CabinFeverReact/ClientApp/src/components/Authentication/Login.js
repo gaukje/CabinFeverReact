52ccc8566374f87c85353bb421a0affffe96acd4
Alex Vu <89573088+s186969@users.noreply.github.com>
2023-11-28 17:17:31 +0100

    Login UI er tilbake

--- CabinFeverReact/ClientApp/src/components/Authentication/Login.js
+++ CabinFeverReact/ClientApp/src/components/Authentication/Login.js
@@ -9,6 +9,8 @@ const Login = () => {
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
+    const [errorMessage, setErrorMessage] = useState('');
+
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
@@ -23,25 +25,71 @@ const Login = () => {
             }
         } catch (error) {
             console.error('Login failed:', error);
+            setErrorMessage('Incorrect e-mail address or password');
+
         }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-                placeholder="Username"
-            />
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-            />
-            <button type="submit">Login</button>
-        </form>
+        <div>
+            <div
+                className="vh-100"
+                style={{
+                    backgroundImage: "url('images/login_background.jpg')",
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center'
+                }}
+            >
+                <div className="row d-flex align-items-center vh-100">
+                    <div className="row justify-content-center">
+                        <div className="col-md-4 m-5 p-4 align-self-start text-center">
+                            <h1 className="text-white">Welcome</h1>
+                            <hr className="text-white" />
+                            <p className="text-white">Here you can log in and see all your details as a guest or a host</p>
+                            <div className="banner-link mb-3">
+                                <a href="/register">Not a member yet? Become one!</a>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4 m-5 p-4 bg-light rounded">
+                            <section>
+                                <h1 className="text-center">Login</h1>
+                                <hr />
+                                <form onSubmit={handleSubmit}>
+                                    <div className="form-floating mb-2">
+                                        <input
+                                            className="form-control"
+                                            type="text"
+                                            value={userName}
+                                            onChange={(e) => setUserName(e.target.value)} />
+                                        <label for="floatingInput">E-mail address</label>
+
+                                    </div>
+                                    <div className="form-floating mb-2">
+                                        <input
+                                            className="form-control"
+                                            type="password"
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)} />
+                                        <label for="floatingInput">Password</label>
+                                    </div>
+                                    <p><span className="text-danger" id="errorMessage">{errorMessage}</span></p>
+
+
+                                    <button
+                                        className="w-100 btn btn-lg btn-primary mb-2"
+                                        type="submit"
+                                    >
+                                        Log in
+                                    </button>
+                                </form>
+                            </section>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     );
 };
 
